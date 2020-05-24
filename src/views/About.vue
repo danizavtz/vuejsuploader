ed67165542ddb744358b7cf6081795518d2a4662
Danizavtz <daninove@gmail.com>
2020-05-24 04:42:16 -0300

    Correcao de atributos

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="about">
     <h1>Uma aplicação para testar varias estratégias de se fazer fileupload utilizando nodejs.</h1>
-    <li><h3>newcss</h3></li>
-    <li><h3>vuejs</h3></li>
-    <li><h3>axios</h3></li>
+    <h3><li>newcss</li></h3>
+    <h3><li>vuejs</li></h3>
+    <h3><li>axios</li></h3>
     
   </div>
 </template>
